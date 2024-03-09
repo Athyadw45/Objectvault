322f8fd296a638f87e075d13751608e5368a2cbe
sdragon <atharvawakodikar699@gmail.com>
2024-03-09 16:21:53 +0530

    intial commit

---
+++ src/main/java/com/objectvault/objectvault/ObjectvaultApplication.java
@@ -0,0 +1,14 @@
+package com.objectvault.objectvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectvaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjectvaultApplication.class, args);
+	}
+	System.out.println("Hello...Atharva Here");
+
+}

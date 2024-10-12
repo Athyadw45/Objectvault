efb56707af517afcd17b0a7ab6f7311d8f9054e7
sdragon <atharvawakodikar699@gmail.com>
2024-10-12 18:13:47 +0530

    created a upload_file button

--- streamlit-ui/pages/page1.py
+++ streamlit-ui/pages/page1.py
@@ -5,7 +5,7 @@ import requests
 make_sidebar()
 
 jwt_token = st.session_state.get('jwt_token')
-uploaded_file = st.file_uploader("Choose a file") 
+uploaded_file = st.file_uploader("Choose a file")
 filesList = None
 
 def list_files():
@@ -25,15 +25,16 @@ def upload_file(uploaded_file):
     endpoint="http://35.208.81.217:3001/v1/upload-file"
     headers={"Authorization":f"Bearer {jwt_token}"}
     if uploaded_file is not None:
-        files = {'file': (uploaded_file.name, uploaded_file.getvalue())}
+        content_type = uploaded_file.type
+        files = {'file': (uploaded_file.name, uploaded_file.getvalue(), content_type)}
         r = requests.post(url=endpoint, headers=headers, files=files)
         if r.status_code != 200:
-            st.warning("File uplaod failed with status code "+str(r.status_code), icon="⚠️")
+            st.warning("File uplaod failed with status code "+str(r.status_code) +"\n"+ str(r.content),icon="⚠️")
             uploaded_file = None
             return 
         
         filesList = list_files()
-        st.info("File uploaded successfully!! \n" + str(r.content),icon="ℹ️")
+        st.info("File uploaded successfully!! " + str(r.content),icon="ℹ️")
         uploaded_file=None
 
 if st.button("Upload File"):
